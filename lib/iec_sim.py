b92cb0d639cd86b520cded67c6b21ecfcef5d248
Nate Miles <ndmiles@Nates-MacBook-Pro.local>
2017-03-18 12:15:31 -0400

    added alternate path for handling lambda_plus and K_plus

--- lib/iec_sim.py
+++ lib/iec_sim.py
@@ -23,35 +23,48 @@ def f(y_,R_,params_):
     Keyword Arguments:
     y_ -- initial conditions
     R_ -- domain of radial coordinates to integrate over 
-	  scaled to bein interval [0,1]
+      scaled to bein interval [0,1]
     params_ -- variety of parameters present in eq 7.
  
     returns the derivate at each point in the specified domain (R_)
     """
-    phi_, E_ = y_
-    ranode_ = params_[0]
-    Vcathode_ = params_[1]
-    rhoi_ = params_[2]
-    I_i = params_[3]
-    I_e  = params_[4]
-    m_e =  params_[5]
-    m_i = params_[6]   
-    try:
-        derivs_ = [E_, -2*E_/R_ + 4 * pi * rhoi_* ranode_**2 * phi_**0.5 \
+    if len(params_) < 3:
+        phi_, E_ = y_
+        K_plus = params_[0]
+        lambda_plus = params_[1]
+        try:
+            derivs_ = [E_, -2*E_/R_ + K_plus/R_**2 * (\
+                       np.sqrt(phi_) - lambda_plus *(1/np.sqrt(1 - phi_)))]
+        except Exception as e:
+            print(e)
+            return e
+        else:
+            return derivs_
+    else:
+        phi_, E_ = y_
+        ranode_ = params_[0]
+        Vcathode_ = params_[1]
+        rhoi_ = params_[2]
+        I_i = params_[3]
+        I_e  = params_[4]
+        m_e =  params_[5]
+        m_i = params_[6]   
+        try:
+            derivs_ = [E_, -2*E_/R_ + 4 * pi * rhoi_* ranode_**2 * phi_**0.5 \
                   * (1/Vcathode_)*(((np.absolute(phi_))**0.5 - (I_e/I_i)) \
                   * (m_e/m_i) * 1/(1-phi_)**0.5)]
-    except Exception as e:
-        return e
-    else:
-        return derivs_
+        except Exception as e:
+            return e
+        else:
+            return derivs_
 
 
 
 def main():
     #Parameter definition
-    phi_ = 0.1
+    phi_ = 0.1 #potential scaled to the potential at outer anode
     E_ = 0.5
-    R_ = np.arange(0.05,1.05,0.01)
+    R_ = np.arange(0.001,1.001,0.001)
     print(R_)
     #radius of the grounded, outer anode(cm) by definition r_a = r/R in eq 7.
     #Hence it attains a value less than one because we are considering ion motion
@@ -66,21 +79,24 @@ def main():
 
     #ion and electron currents
     I_i = 0.00235
-    I_e = 0.00003452
+    I_e = 0.003452
 
     #electron and ion masses
     m_e = _m_e[0] 
     m_i = _m_p[0]
     y0 = [phi_, E_]
-    params_= [ranode_,Vcathode_,rho_i,I_i,I_e,m_e,m_i]
+    params_ = [0.5, 0.3]
+   # params_= [ranode_,Vcathode_,rho_i,I_i,I_e,m_e,m_i]
     print(params_)
 
     psoln = odeint(f,y0,R_,args=(params_,))
 
-    print(psoln)
+    print(len(psoln[:,0]) -len(R_))
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
-    ax.plot(R_,psoln[:,0])
+   # ax.plot(R_,psoln[:,1],'-b',label='-dPhi/dR',linewidth=3.5)
+    ax.plot(R_,psoln[:,0],'-g',label='Phi',linewidth=1.5)
+    plt.legend(loc=1)   
     plt.show()
 
 if __name__ == "__main__": 
